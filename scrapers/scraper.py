817d9466ab457257ed00717ab1234c0282b468a1
Sai-96 <khansymon@gmail.com>
2025-08-13 00:06:05 +0100

    Created function to extract all URLs to pages that hold relevant data

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -6,12 +6,13 @@ import json
 import os
 import pandas as pd
 
-# Set the url
+# Set the url and headers
 url = "https://obr.uk/efo"
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0"}
 
 
 # Create function to send GET requests to URLs and create soup
-def get_url_soup(url: str) -> BeautifulSoup:
+def get_url_soup(url: str, headers:str) -> BeautifulSoup:
     """
     This function sends GET requests to given URLs for web scraping purposes. 
 
@@ -24,7 +25,6 @@ def get_url_soup(url: str) -> BeautifulSoup:
 
     # The url rejects GET requests that do not specify a user agent. As a result, it returns 403 forbidden. 
     # # Fetching user agent from developer tools from browser. F12 -> network -> click on first one listed -> look for user agent under headers. 
-    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0"}
 
     # Set up the session
     session = requests.Session()
@@ -39,13 +39,52 @@ def get_url_soup(url: str) -> BeautifulSoup:
     return BeautifulSoup(response.text, "html.parser")
 
 
-soup = get_url_soup(url)
+soup = get_url_soup(url, headers = headers)
+
+def extract_urls(url:str, soup: BeautifulSoup) -> list:
+    """
+    This function takes a BeautifulSoup as an input.
+    It's tailored to specifically look at the OBR's Economic Forecast Outlook page
+    and extract all the links of past EFO publications. 
+    Main reason for doing this is that we want all the pages that contain data we're interested in. 
+
+    Args:
+        url (string): include the initial url to add to the final list. 
+        soup (BeautifulSoup): BeatifulSoup for the url supplied.
+
+    Returns:
+        list: list of all URLs that contain data of interest. 
+    """
+
+    # Set the set to add the URLs to. Creating a set to ensure no dupes. Will turn into list later. 
+    URLs = set()
+
+    options = soup.find_all("option")
+
+    # May contain None so adding all of them first
+    for option in options:
+        URLs.add(option.get("value"))
+
+    # Now removing None and keeping only the URLs I am interested in. 
+    return [URL for URL in list(URLs) if URL is not None and "economic-and-fiscal-outlook" in URL]
+
+urls = extract_urls(url = url, soup = soup)
+
+
+
+
+
+
+
+
+
+
+
+
 
 # After some inspection, I am interested in the detailed forecasts for the economy
 # Only need to extract the urls associated with a download link. 
 links_to_docs = soup.find_all("a", class_ = "download-link")
-
-print(soup.find_all("option"))
 
 # Create a set to store unique urls
 link_of_interest = set()
